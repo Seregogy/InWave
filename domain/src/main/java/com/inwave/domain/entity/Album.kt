eb1f377d864c914ffcc473de04e9e6b32f2a0a47
Babayka26 <14maks2007@gmail.com>
2025-11-06 00:21:53 +0300

    Создание Album.kt и определение в нем data class Album

---
+++ domain/src/main/java/com/inwave/domain/entity/Album.kt
@@ -0,0 +1,11 @@
+data class Album (
+    val id: String,
+    val name: String,
+    val imageUrl: String,
+    val artists: List<String>,
+    val likes: String,
+    val listenting: String,
+    val releaseDate: Long,
+    val genre: String,
+    val label: String,
+)
