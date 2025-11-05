58279dee3c9b5011b040e359fc1a86140b005cfc
Nuts31523 <bahbolshakov@gmail.com>
2025-11-05 22:19:52 +0300

    refactor Track path

--- domain/entity/Track/src/Track.kt
+++
@@ -1,2 +0,0 @@
-data class Lyrics(val plainText : String, val syncedText : Map<Long, String>, val provider : String)
-data class Track(val id : String, val name : String, val imageUrl : String, val indexInAlbum : Int, val duration : Int, val lyrics : String, val listening : String, val audioUrl : String, val album : String)
