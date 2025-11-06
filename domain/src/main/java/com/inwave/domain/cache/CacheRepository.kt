764206632d42e4dd79ba7423ae8df2c5b9cbd311
Seregogy <delhikovsr@gmail.com>
2025-11-06 21:11:36 +0300

    refactor cache repository

--- domain/src/main/java/com/inwave/domain/cache/CacheRepository.kt
+++ domain/src/main/java/com/inwave/domain/cache/CacheRepository.kt
@@ -1,7 +1,7 @@
 package com.inwave.domain.cache
 
 interface CacheRepository<Key, Data> {
-    suspend fun put(key: Key, data: Data): Boolean
+    suspend fun put(data: Pair<Key, Data>): Boolean
     suspend fun get(key: Key): Data?
     suspend fun remove(key: Key): Boolean
 }
