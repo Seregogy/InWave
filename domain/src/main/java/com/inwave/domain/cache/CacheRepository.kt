d70d6a1ce6c961959da956d16dec5f87bc0786a4
Seregogy <delhikovsr@gmail.com>
2025-11-06 22:53:58 +0300

    add contains method in cache repo

--- domain/src/main/java/com/inwave/domain/cache/CacheRepository.kt
+++ domain/src/main/java/com/inwave/domain/cache/CacheRepository.kt
@@ -3,5 +3,6 @@ package com.inwave.domain.cache
 interface CacheRepository<Key, Data> {
     suspend fun put(data: Pair<Key, Data>): Boolean
     suspend fun get(key: Key): Data?
+    suspend fun contains(key: Key): Boolean
     suspend fun remove(key: Key): Boolean
 }
