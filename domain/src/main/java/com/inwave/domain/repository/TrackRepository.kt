b746fbdf9aca1864f90d277bba63c67d2279dfa1
Seregogy <delhikovsr@gmail.com>
2025-11-06 21:12:07 +0300

    refactor track repository result return value

--- domain/src/main/java/com/inwave/domain/repository/TrackRepository.kt
+++ domain/src/main/java/com/inwave/domain/repository/TrackRepository.kt
@@ -1,5 +1,6 @@
 package com.inwave.domain.repository
 
+import com.inwave.domain.entity.Lyrics
 import com.inwave.domain.entity.Track
 
 interface TrackRepository {
@@ -7,7 +8,7 @@ interface TrackRepository {
     suspend fun getTracks(ids: List<String>): Result<List<Track>>
     suspend fun getRandomTrack(): Result<Track>
     suspend fun getRandomTrackId(): Result<String>
-    suspend fun getTrackLyrics(id: String): Result<String>
+    suspend fun getTrackLyrics(id: String): Result<Lyrics>
     suspend fun toggleLike(id: String): Result<Boolean>
     suspend fun searchTracks(query: String, limit: Int = 10): Result<List<Track>>
 }
