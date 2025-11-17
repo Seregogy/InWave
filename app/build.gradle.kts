093f0a36f3106075db64a8820613fa4670fbe1f4
Seregogy <delhikovsr@gmail.com>
2025-11-17 21:39:25 +0300

    add hilt, media3 exo player and appyx navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("kotlin-kapt")
+    alias(libs.plugins.hilt)
+}
+
+kapt {
+    correctErrorTypes = true
 }
 
 android {
@@ -60,6 +67,14 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    kapt(libs.hilt.compiler)
+
+    implementation(libs.androidx.media3.exoplayer)
+
+    implementation(libs.appyx.core)
 
     implementation(libs.coil.compose)
     implementation(libs.accompanist.permissions)
