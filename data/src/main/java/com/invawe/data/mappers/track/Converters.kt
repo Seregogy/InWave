01ba0fa69b7ae3d1fc9c76feaa9394382347f89b
Seregogy <delhikovsr@gmail.com>
2025-12-05 17:23:46 +0300

    add toDomainTrack mapper

--- data/src/main/java/com/invawe/data/mappers/track/Converters.kt
+++ data/src/main/java/com/invawe/data/mappers/track/Converters.kt
@@ -1,6 +1,9 @@
 package com.invawe.data.mappers.track
 
+import android.content.ContentUris
+import android.database.Cursor
 import android.net.Uri
+import android.provider.MediaStore
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MediaMetadata
 import com.inwave.domain.entity.Track
@@ -19,3 +22,30 @@ fun Track.toMediaItem(): MediaItem = MediaItem.Builder()
                 .build()
         )
         .build()
+
+fun Cursor.toDomainTrack(): Track {
+    val audioIdColumn = this.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+    val trackNameColumn = this.getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.TITLE)
+    val durationColumn = this.getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.DURATION)
+
+    val trackName = this.getString(trackNameColumn)
+    val duration = this.getLong(durationColumn)
+    val audioId = this.getLong(audioIdColumn)
+
+    val audioUri = ContentUris.withAppendedId(
+        MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+        audioId
+    )
+
+    return Track(
+        "",
+        trackName,
+        "",
+        0,
+        duration,
+        null,
+        0,
+        audioUri.toString(),
+        null
+    )
+}
