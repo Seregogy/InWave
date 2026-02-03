ce5c643706ff4567c7c62aae9a9c6a5903747a83
Seregogy <delhikovsr@gmail.com>
2026-02-03 22:13:11 +0300

    added base dependencies

--- backend/main/build.gradle.kts
+++ backend/main/build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
 	alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    application
 }
 
 group = "com.inwave"
 version = "0.0.1"
+
+application {
+    mainClass.set("com.inwave.backend.MainKt")
+}
 
 tasks.test {
 	useJUnitPlatform()
@@ -16,4 +22,10 @@ kotlin {
 dependencies {
     implementation(project(":domain"))
 	testImplementation(kotlin("test"))
+    testImplementation(libs.ktor.server.test.host)
+
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.config.yaml)
+    implementation(libs.logback.classic)
 }
--- backend/main/src/main/kotlin/Main.kt
+++ backend/main/src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
-package com.inwave
+package com.inwave.backend
+
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
 
 fun main() {
-    println("Hello from InWave-Backend")
+    embeddedServer(Netty, port = 8000) {
+        routing {
+            get("/") {
+                call.respondText("Hey from InWave-Backend!")
+            }
+        }
+    }.start(wait = true)
 }
