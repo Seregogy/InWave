9fcf66cfb210abb15f349bf519a3e3d2ef9223e9
Babayka26 <14maks2007@gmail.com>
2026-03-01 19:26:32 +0300

    Создание TrackRepositorylmpt

---
+++ backend/data/src/main/java/com/inwave/backend/data/repository/track/TrackRepositoryImpl.kt
@@ -0,0 +1,89 @@
+package com.inwave.backend.data.repository.track
+
+import com.inwave.backend.data.map.toDomain
+import com.inwave.backend.data.repository.catchingTransaction
+import com.inwave.backend.db.entity.TrackEntity
+import com.inwave.backend.db.table.TrackTable
+import com.inwave.domain.entity.Track
+import com.inwave.domain.entity.Track.Lyrics
+import com.inwave.domain.repository.TrackRepository
+import org.jetbrains.exposed.v1.core.Random
+import org.jetbrains.exposed.v1.core.dao.id.EntityID
+import org.jetbrains.exposed.v1.core.like
+import org.jetbrains.exposed.v1.jdbc.Database
+import org.jetbrains.exposed.v1.jdbc.select
+import org.jetbrains.exposed.v1.jdbc.selectAll
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+
+class TrackRepositoryImpl(
+    private val db: Database
+) : TrackRepository {
+    override suspend fun getTrack(id: String): Result<Track> = catchingTransaction(db) {
+        TrackEntity.findById(id.toInt())!!.toDomain()
+    }
+
+    override suspend fun getTracks(ids: List<String>): Result<List<Track>> = catchingTransaction(db) {
+        ids.map { EntityID(it.toInt(), TrackTable) }.let { entityIDS ->
+            TrackEntity.forEntityIds(entityIDS).map { it.toDomain() }
+        }
+    }
+
+    override suspend fun getAllTracks(
+        page: Int,
+        size: Int
+    ): Result<List<Track>> = catchingTransaction(db) {
+        TrackTable.selectAll()
+            .drop(page * size)
+            .take(size).map {
+                TrackEntity.wrapRow(it)
+                    .toDomain()
+            }
+    }
+
+
+    override suspend fun getRandomTrack(): Result<Track> = catchingTransaction(db) {
+        TrackTable.selectAll()
+            .orderBy(Random())
+            .limit(1)
+            .first().let {
+                TrackEntity.wrapRow(it).toDomain()
+            }
+    }
+
+
+    override suspend fun getRandomTrackId(): Result<String> = catchingTransaction(db) {
+        TrackTable.select(TrackTable.id)
+            .orderBy(Random())
+            .limit(1)
+            .first()[TrackTable.id].value.toString()
+    }
+
+    override suspend fun getTrackLyrics(id: String): Result<Lyrics> = catchingTransaction(db) {
+        TrackEntity.findById(id.toInt())!!
+            .fetchLyrics()!!
+            .toDomain()
+    }
+
+
+    override suspend fun getTrackWithLyrics(id: String): Result<Track> = catchingTransaction(db) {
+        TrackEntity.findById(id.toInt())!!.toDomain()
+    }
+
+    override suspend fun toggleLike(id: String): Result<Boolean> {
+        TODO("User entity not implemented yet")
+    }
+
+    override suspend fun searchTracks(
+        query: String,
+        limit: Int
+    ): Result<List<Track>> = catchingTransaction(db) {
+        val escaped = query.replace("%", "\\%")
+            .replace("_", "\\_")
+
+        TrackEntity.find {
+            TrackTable.name like "%$escaped%"
+        }.limit(limit).map {
+            it.toDomain()
+        }
+    }
+}
