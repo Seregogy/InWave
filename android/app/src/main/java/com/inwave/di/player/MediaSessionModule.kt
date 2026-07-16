eb66c8f7ab77d6733cc4dbc8d655608e55d14d55
Sergogy <delhikovsr@gmail.com>
2026-07-16 17:23:08 +0300

    added mediaSessionModule

---
+++ android/app/src/main/java/com/inwave/di/player/MediaSessionModule.kt
@@ -0,0 +1,31 @@
+package com.inwave.di.player
+
+import androidx.annotation.OptIn
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.session.MediaController
+import com.inwave.di.RemoteLegacyRepo
+import com.inwave.player.state.PlayerStateHandler
+import com.inwave.player.state.PlayerStateSource
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.scopes.ServiceScoped
+
+@Module
+@InstallIn(ServiceComponent::class)
+object MediaSessionModule {
+    @OptIn(UnstableApi::class)
+    @Provides
+    @ServiceScoped
+    @RemoteLegacyRepo
+    fun providePlayerStateHandler(
+        @RemoteLegacyRepo playerStateSource: PlayerStateSource,
+        mediaController: MediaController
+    ): PlayerStateHandler {
+        return PlayerStateHandler(
+            playerStateSource,
+            mediaController
+        )
+    }
+}
